0ad1380d8e7c2a1622940093fc0982bfa7cef5dc
Erik <etchao001@gmail.com>
2020-12-10 10:11:03 -0800

    Added functions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,22 @@
 FrogEscapeApp = {
     init: function() {
+        // window.onkeydown = function() {}
+    },
+    createBubba: function() {
+
+    },
+    animateBubba: function() {
+
+    },
+
+    createLilypad: function() {
+
+    },
+
+    checkforLilypad: function() {
 
     }
+
+
 }
 FrogEscapeApp.init();
