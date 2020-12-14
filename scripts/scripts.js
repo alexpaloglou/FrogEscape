b80afa5a7f097da288ee7fd968bebdd5b889d515
Erik <etchao001@gmail.com>
2020-12-14 12:52:07 -0800

    Updated init and createLilypad function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,14 @@
 FrogEscapeApp = {
+
+    
+    lilypad: [],
+
     init: function() {
+        for (let i = 1; i < 100; i++) {
+            this.lilypad.push(this.createLilypad());
+        };
+        this.lilypad[0].frogOn = true;
+
         // window.onkeydown = function() {}
     }, 
     createBubba: function() {
@@ -10,7 +19,14 @@ FrogEscapeApp = {
     },
 
     createLilypad: function() {
-
+        let lilypaddiv = document.createElement("div");
+        lilypaddiv.className = "lilypad";
+        this.container.append(lilypaddiv);
+        let lilypad = {
+            frogOn: false,
+            x_velocity: 0,
+            y_velocity: 0
+        }
     },
 
     checkforLilypad: function() {
