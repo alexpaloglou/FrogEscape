9be10817c7a8fe0c7778be0fec6b4d6d781feb41
Erik <etchao001@gmail.com>
2021-01-08 14:09:28 -0800

    Fixed removeLilypad (needs work)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,12 +12,22 @@ FrogEscapeApp = {
     container: document.getElementById("frog_pond"),
 
     init: function () {
-        for (let i = 0; i < 10; i++) {
-            this.lilypads.push(this.createLilypad());
+        for (let i = 0; i < 4; i++) {
+            this.lilypads.push(this.createLilypad(0))
+        
         }
+        this.lilypads[0].x_pos = 35
+            this.lilypads[2].x_pos = 285
+
+            this.lilypads[3].x_pos = 410
         
         // window.onkeydown = function() {}
         this.startFrogEscape();
+
+    },
+
+    startFrogEscape: function () {
+        this.game = window.setInterval(this.animateGame.bind(FrogEscapeApp), 30);
 
     },
 
@@ -27,24 +37,26 @@ FrogEscapeApp = {
         this.renderLilypads();
         this.removeLilypads();
         // this.animateBubba();
-        this.checkBubbaPosition();
+       // this.checkBubbaPosition();
     },
 
-    createLilypad: function () {
+    createLilypad: function (lane) {
         console.log("Creating Lilypads.")
         let lilypaddiv = document.createElement("div");
         lilypaddiv.className = "lilypad";
-        this.container.append(lilypaddiv)
-
+        FrogEscapeApp.lanes[lane].append(lilypaddiv);
+        
         // Randomly decide which row 0,1,2,3
         // Append to that chosen row
         // Set the vertical position to the top
-
+        // Append all lilypads to one lane (test)
+        // Make x_pos: 35, 160, 285, 410
 
         let lilypad = {
-            x_pos: 0,
+            x_pos: 160,
             y_pos: 10,
-            y_veloctiy: 10,
+            x_velocity: 0,
+            y_velocity: 3,
 
             element: lilypaddiv,
         }
@@ -58,12 +70,15 @@ FrogEscapeApp = {
         console.log("Moving Lilypads")
         for (let i = 0; i < this.lilypads.length; i++) {
             this.lilypads[i].y_pos = this.lilypads[i].y_pos + this.lilypads[i].y_velocity;
+            this.lilypads[i].x_pos = this.lilypads[i].x_pos + this.lilypads[i].x_velocity;
         }
     },
 
     renderLilypads: function () {
+        console.log("Rendering Lilypads");
         for (let i = 0; i < this.lilypads.length; i++) {
             this.lilypads[i].element.style.top = this.lilypads[i].y_pos + "px";
+            this.lilypads[i].element.style.left = this.lilypads[i].x_pos + "px";
         }
     },
 
@@ -71,12 +86,17 @@ FrogEscapeApp = {
 
     removeLilypads: function () {
           console.log("Removing any Lilypads that needs to be removed.")
-          let lilypad = document.getElementsByClassName('lilypad');
             for (let i = 0; i < this.lilypads.length; i++) {
-                if (this.lilypads[i].y_pos = 700) {
-                lilypad.parentNode.removeChild(lilypad);
+                if (this.lilypads[i].x_pos != null) {
+                    if (this.lilypads[i].y_pos >= 700) {
+                    this.lanes[0].removeChild(this.lilypads[i].element)
+                   
+                
             }
-            }
+        }
+    }
+            
+            
         
         //  console.log("Removing any Lilypads that needs to be removed.")
     },
@@ -87,10 +107,6 @@ FrogEscapeApp = {
 
     checkBubbaPosition: function () {
         //  console.log("Checking Bubba's position.")
-    },
-
-    startFrogEscape: function () {
-        this.game = window.setInterval(this.animateGame.bind(FrogEscapeApp), 30);
     },
 
 
